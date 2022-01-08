0b74a92196ee86696425c89ca3009a346c685ef2
vits56 <bastosduartebruno@gmail.com>
2022-01-08 02:31:42 -0300

    hook criado

--- src/components/NewTransactionModal/index.tsx
+++ src/components/NewTransactionModal/index.tsx
@@ -1,10 +1,16 @@
-import { FormEvent, useState, useContext } from 'react';
+import { FormEvent, useState } from 'react';
 import Modal from 'react-modal';
+import { useTransactions } from '../../hooks/useTransactions';
+
+
 import closeImg from '../../assets/close.svg'
 import incomeImg from '../../assets/income.svg'
 import outcomeImg from '../../assets/outcome.svg'
-import { TransactionsContext } from '../../TransactionsContext';
+
+
 import { Container, TransactionTypeContainer, RadioBox } from './styles';
+
+
 
 interface NewTransactionModalProps {
   isOpen: boolean;
@@ -12,7 +18,7 @@ interface NewTransactionModalProps {
 }
 
 export function NewTransactionModal({ isOpen, onRequestClose }: NewTransactionModalProps) {
-  const { createTransaction } = useContext(TransactionsContext);
+  const { createTransaction } = useTransactions();
 
   const [title, setTitle] = useState('');
   const [amount, setAmount] = useState(0);
